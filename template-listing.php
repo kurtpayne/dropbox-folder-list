68ec7c49f6aa5fc70c4d4e8d4b50c787e04a6abe
kurtpayne <kpayne@gmail.com>
2013-08-24 00:20:30 -0700

    Get download links working. Use home_url() instead of REQUEST_URI

--- template-listing.php
+++ template-listing.php
@@ -5,14 +5,14 @@
 		<?php foreach( $files['contents'] as $file ) : ?>
 			<?php if ( $file['is_dir'] ) : ?>
 				<li class="<?php echo $file['icon']; ?>">
-					<a href="<?php echo str_replace( '?' . $_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI'] ) . basename( $file['path'] ); ?>"><?php echo basename( $file['path'] ) ;?></a>
+					<a href="<?php echo home_url( $file['path'] ); ?>"><?php echo basename( $file['path'] ) ;?></a>
 				</li>
 			<?php endif; ?>
 		<?php endforeach; ?>
 		<?php foreach( $files['contents'] as $file ) : ?>
 			<?php if ( !$file['is_dir'] ) : ?>
 				<li class="<?php echo $file['icon']; ?>">
-					<a href="#"><?php echo basename( $file['path'] ) ;?></a> (<?php echo $file['size']; ?>)
+					<a href="<?php echo home_url( 'dropbox' . $file['path'] ); ?>"><?php echo basename( $file['path'] ) ;?></a> (<?php echo $file['size']; ?>)
 				</li>
 			<?php endif; ?>
 		<?php endforeach; ?>

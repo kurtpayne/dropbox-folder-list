fe6db8003160192a79eb65339c21a7108ced8dfd
kurtpayne <kpayne@gmail.com>
2013-08-24 00:01:05 -0700

    List folders first, then files.

--- template-listing.php
+++ template-listing.php
@@ -2,14 +2,19 @@
 	<?php _e( 'No files in this folder', 'dfc_plugin' ); ?>
 <?php else : ?>
 	<ul class="dfc-file-listing">
-	<?php foreach( $files['contents'] as $file ) : ?>
-		<li class="<?php echo $file['icon']; ?>">
+		<?php foreach( $files['contents'] as $file ) : ?>
 			<?php if ( $file['is_dir'] ) : ?>
-				<a href="<?php echo str_replace( '?' . $_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI'] ) . basename( $file['path'] ); ?>"><?php echo basename( $file['path'] ) ;?></a>
-			<?php else : ?>
-				<a href="#"><?php echo basename( $file['path'] ) ;?></a> (<?php echo $file['size']; ?>)
+				<li class="<?php echo $file['icon']; ?>">
+					<a href="<?php echo str_replace( '?' . $_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI'] ) . basename( $file['path'] ); ?>"><?php echo basename( $file['path'] ) ;?></a>
+				</li>
 			<?php endif; ?>
-		</li>
-	<?php endforeach; ?>
+		<?php endforeach; ?>
+		<?php foreach( $files['contents'] as $file ) : ?>
+			<?php if ( !$file['is_dir'] ) : ?>
+				<li class="<?php echo $file['icon']; ?>">
+					<a href="#"><?php echo basename( $file['path'] ) ;?></a> (<?php echo $file['size']; ?>)
+				</li>
+			<?php endif; ?>
+		<?php endforeach; ?>
 	</ul>
 <?php endif; ?>
